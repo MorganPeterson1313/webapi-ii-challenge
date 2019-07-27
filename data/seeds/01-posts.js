0c432fc2c9fbe9838fc660317df53dd8a14d0e9e
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-07-26 23:17:38 -0400

    making front end react instagram clone app 2 for back end using redux

--- data/seeds/01-posts.js
+++ data/seeds/01-posts.js
@@ -7,45 +7,63 @@ exports.seed = function(knex, Promise) {
           title:
             'I wish the ring had never come to me. I wish none of this had happened.',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title: 'I think we should get off the road. Get off the road! Quick!',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             "I made a promise, Mr Frodo. A promise. \"Don't you leave him Samwise Gamgee.\" And I don't mean to. I don't mean to.",
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             " N-nothing important. That is, I heard a great deal about a ring, a Dark Lord, and something about the end of the world, but... Please, Mr. Gandalf, sir, don't hurt me. Don't turn me into anything... unnatural.",
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             'You need people of intelligence on this sort of mission...quest...thing.',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             'All you have to do is decide what to do with the time that is given to you.',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             'Do not be so quick to deal out death and judgement. Even the very wise do not see all ends.',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             ' Fool of a Took! Throw yourself in next time and rid us of your stupidity!',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
         {
           title:
             'I will be dead before I see the ring in the hands of an elf! Never trust an elf!',
           contents: 'Guess who said this',
+          thumbnailUrl:'',
+          imageUrl:'',
         },
       ]);
     });
