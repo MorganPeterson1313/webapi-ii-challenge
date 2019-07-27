9948cf04a7d3549f1cc4ad6afe21d7d6d6248444
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-07-27 13:41:45 -0400

    changed placeemnt of cors import to server.js file

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
 const server = require('./server');
-var cors = require('cors')
-server.use(cors())
 
-server.get('/api/posts', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
 
 
 // server.use('api/posts', server);
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const postsRouter = require('./post-router');
 const commentsRouter = require('./comment-router');
 const server = express();
 server.use(express.json());
+const cors = require('cors')
+server.use(cors())
+
+server.get('/api/posts', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+
+
 
 server.use('/api/posts', postsRouter);
 server.use('/api/posts', commentsRouter);
